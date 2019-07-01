89af49cc0198679de0a21c6e6df84eabc94dd6c2
Michael Thomas <Michael@undersea.local>
2019-07-01 17:25:31 +0100

    end of day

---
+++ src/components/BookList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+    render() {
+        return (
+            <div className="book-list">
+                <ul>
+                    <li>great book</li>
+                    <li>some stuff</li>
+                    <li>mega lols</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default BookList;
